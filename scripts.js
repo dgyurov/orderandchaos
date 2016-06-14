f21957ba06f462e5c79deea0d60f6375e97ca53f
Dimitar Gyurov <dgyurov@gmail.com>
2016-06-14 21:21:51 +0200

    Almost there.

---
+++ scripts.js
@@ -0,0 +1,149 @@
+$(function () {
+
+    function scrollTo(el) {
+        var elOffset = el.offset().top;
+        var elHeight = el.height();
+        var windowHeight = $(window).height();
+        var offset;
+
+        if (elHeight < windowHeight) {
+            offset = elOffset - ((windowHeight / 2) - (elHeight / 2));
+        } else {
+            offset = elOffset;
+        }
+
+        $('html,body').animate({
+            scrollTop: offset
+        });
+    }
+    if ($(this).scrollTop() > 350) {
+        $('#btnTop').fadeIn();
+    } else {
+        $('#btnTop').fadeOut();
+    }
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 350) {
+            $('#btnTop').fadeIn();
+        } else {
+            $('#btnTop').fadeOut();
+        }
+    });
+
+    $("a#toggle").click(function () {
+        $("#popmenu").slideToggle();
+        return false;
+    });
+
+    $("li#cart-action").click(function () {
+        scrollTo($(".cart-box"));
+        return false;
+    });
+
+    $("li#dream-action").click(function () {
+        scrollTo($(".dream-box"));
+        return false;
+    });
+
+    $("li#exp-action").click(function () {
+        scrollTo($(".exp-box"));
+        return false;
+    });
+    $("li#gem-action").click(function () {
+        scrollTo($(".gem-box"));
+        return false;
+    });
+
+    $("li#vigor-action").click(function (e) {
+        scrollTo($(".vigor-box"));
+        return false;
+    });
+
+    $("a#btnTop").click(function (e) {
+        scrollTo($(".weapons-list"));
+        return false;
+    });
+
+    // Sword
+    $(".cart-bg").on("mouseover", function () {
+        $(".sword").css("background-image", "url(images/sword_1.png)");
+    }).mouseout(function () {
+        $(".sword").css("background-image", "url(images/sword_0.png)");
+    });
+    $(".dream-bg").on("mouseover", function () {
+        $(".sword").css("background-image", "url(images/sword_2.png)");
+        $(".cart-bg").toggleClass('cart-bg-hover');
+    }).mouseout(function () {
+        $(".sword").css("background-image", "url(images/sword_0.png)");
+        $(".cart-bg").toggleClass('cart-bg-hover');
+    });
+    $(".exp-bg").on("mouseover", function () {
+        $(".sword").css("background-image", "url(images/sword_3.png)");
+        $(".cart-bg").toggleClass('cart-bg-hover');
+        $(".dream-bg").toggleClass('dream-bg-hover');
+    }).mouseout(function () {
+        $(".sword").css("background-image", "url(images/sword_0.png)");
+        $(".cart-bg").toggleClass('cart-bg-hover');
+        $(".dream-bg").toggleClass('dream-bg-hover');
+    });
+    $(".gem-bg").on("mouseover", function () {
+        $(".sword").css("background-image", "url(images/sword_4.png)");
+        $(".cart-bg").toggleClass('cart-bg-hover');
+        $(".dream-bg").toggleClass('dream-bg-hover');
+        $(".exp-bg").toggleClass('exp-bg-hover');
+    }).mouseout(function () {
+        $(".sword").css("background-image", "url(images/sword_0.png)");
+        $(".cart-bg").toggleClass('cart-bg-hover');
+        $(".dream-bg").toggleClass('dream-bg-hover');
+        $(".exp-bg").toggleClass('exp-bg-hover');
+    });
+    $(".vigor-bg").on("mouseover", function (e) {
+        $(".sword").css("background-image", "url(images/sword_5.png)");
+        $(".cart-bg").toggleClass('cart-bg-hover');
+        $(".dream-bg").toggleClass('dream-bg-hover');
+        $(".exp-bg").toggleClass('exp-bg-hover');
+        $(".gem-bg").toggleClass('gem-bg-hover');
+    }).mouseout(function () {
+        $(".sword").css("background-image", "url(images/sword_0.png)");
+        $(".cart-bg").toggleClass('cart-bg-hover');
+        $(".dream-bg").toggleClass('dream-bg-hover');
+        $(".exp-bg").toggleClass('exp-bg-hover');
+        $(".gem-bg").toggleClass('gem-bg-hover');
+    });
+
+    $(".signup-button").click(function () {
+        $(".popup-container").css('display', 'flex');
+        $(".signup-box").show();
+        return false;
+    });
+
+    $(".login-button").click(function () {
+        $(".popup-container").css('display', 'flex');
+        $(".login-box").show();
+        return false;
+    });
+
+    $(".close-btn").click(function () {
+        $(".popup-container").css('display', 'none');
+        $(".login-box").hide();
+        $(".signup-box").hide();
+        return false;
+    });
+    window.sr = ScrollReveal({
+        reset: true,
+        origin: 'left',
+        distance: '100px',
+        duration: 600,
+    });
+    sr.reveal('.imgrev');
+    sr.reveal('.textrev', {
+        origin: 'right'
+    });
+    sr.reveal('.gameloft-logo', {
+        origin: 'top',
+    });
+    sr.reveal('.social-btns', {
+        reset: true,
+        origin: 'top',
+    });
+});
